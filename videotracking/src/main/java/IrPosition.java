537607580ec5da98abddf7e96e1ff780688cf86b
pop <pop1104@iii.org.tw>
2017-11-20 17:02:04 +0800

    [Modify] Revert java files.

---
+++ videotracking/src/main/java/IrPosition.java
@@ -0,0 +1,12 @@
+
+public class IrDetect {
+
+    public native static boolean importYMLCameraParameters(String filename);
+
+    public native static boolean importYMLDetectParameters(String filename);
+
+    public native static boolean importYMLRTParameters(String filename);
+
+    public native static IrArucoMarker[] findArucoMarkersWithMarkerSize(byte[] bytes, int width,int height, float markerSizeInMeter, float distanceBelowMarkerCenter);
+
+}
