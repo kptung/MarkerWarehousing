385d633cb2156e7560025777c4ccee83ed21504c
kptung <kptung@aa.bb.cc>
2018-01-04 19:18:27 +0800

    [Modify] Add draw circle on java 2D drawer.

--- drawer/src/main/java/org/iii/snsi/drawer/DrawOnCameraFrame.java
+++ drawer/src/main/java/org/iii/snsi/drawer/DrawOnCameraFrame.java
@@ -22,6 +22,7 @@ import java.util.Map;
 public class DrawOnCameraFrame extends DrawTrackingRect {
 
     private HashMap<Integer, int[]> rectSets;
+    private HashMap<Integer, int[]> circleSets;
     private int layoutWidth;
     private int layoutHeight;
     private Bitmap bbitmap;
@@ -30,6 +31,7 @@ public class DrawOnCameraFrame extends DrawTrackingRect {
     public DrawOnCameraFrame(Context context) {
         super(context);
         rectSets = new HashMap<Integer, int[]>();
+        circleSets = new HashMap<Integer, int[]>();
     }
 
     public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
@@ -85,25 +87,29 @@ public class DrawOnCameraFrame extends DrawTrackingRect {
 
             if ((top > layoutHeight) || (bottom < 0) || (right < 0) || (left > layoutWidth))
             {
-                //                System.out.println("not added : " + data[i]);
-                //                System.out.println("leftR = " + leftR);
-                //                System.out.println("topR = " + topR);
-                //                System.out.println("bottomR = " + bottomR);
-                //                System.out.println("leftR = " + leftR);
             } else {
-                //                System.out.println("add : " + data[i]);
-                //                System.out.println("leftR = " + leftR);
-                //                System.out.println("topR = " + topR);
-                //                System.out.println("bottomR = " + bottomR);
-                //                System.out.println("leftR = " + leftR);
-                //                System.out.println("leftL = " + leftL);
-                //                System.out.println("rightL = " + rightL);
-
                 rectSets.put((int) Math.round(data[i]), new int[] {(int) Math.round(left),
                         (int) Math.round(top), (int) Math.round(right),
                         (int) Math.round(bottom)});
-
             }
+        }
+        int[] xx = new int [1];
+        return xx;
+    }
+
+    public int[] processTrackingCircle(int width, int height, double[] data) {
+        rectSets.clear();
+        if (data == null) {
+            return null;
+        }
+
+        for (int i = 0; i < data.length; i += 5) {
+            double left = (double) ((data[i + 1]) / width) * layoutWidth;
+            double top = (double) ((data[i + 2]) / height) * layoutHeight;
+            double radius = (double)  ((data[i + 3]) / width) * layoutWidth;
+            circleSets.put((int) Math.round(data[i]), new int[] {(int) Math.round(left),
+                        (int) Math.round(top), (int) Math.round(radius)});
+
         }
         int[] xx = new int [1];
         return xx;
@@ -132,8 +138,11 @@ public class DrawOnCameraFrame extends DrawTrackingRect {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        //paint.setFilterBitmap(true);
-        canvas.drawBitmap(bbitmap, 0, 0, paint);
+        paint.setFilterBitmap(true);
+        if(bbitmap!=null)
+        {
+            canvas.drawBitmap(bbitmap, 0, 0, paint);
+        }
         paint.setAntiAlias(true);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
@@ -146,11 +155,20 @@ public class DrawOnCameraFrame extends DrawTrackingRect {
             } else {
                 paint.setColor(Color.argb(128, 255, 0, 255));
             }
-
-            drawFilledRect(canvas, paint, new Point(rectData[0], rectData[1]),
-                    new Point(rectData[2], rectData[3]));
+            drawFilledRect(canvas, paint, new Point(rectData[0], rectData[1]), new Point(rectData[2], rectData[3]));
+            //canvas.drawCircle(rectData[0], rectData[1], 30, paint);
         }
-
+        for (Map.Entry entry : circleSets.entrySet()) {
+            Object key = entry.getKey();
+            int[] circleData = circleSets.get(key);
+            if (key.equals(0)) {
+                paint.setColor(Color.argb(128, 0, 255, 255));
+            } else {
+                paint.setColor(Color.argb(128, 255, 0, 255));
+            }
+            canvas.drawCircle(circleData[0], circleData[1], circleData[2], paint);
+            //drawFilledCircle(canvas, paint, new Point(circleData[0], circleData[1]), circleData[2]);
+        }
     }
 
     private void drawFilledRect(Canvas canvas, Paint paint, Point topLeftPoint,
@@ -160,6 +178,14 @@ public class DrawOnCameraFrame extends DrawTrackingRect {
                 bottomRightPoint.y, paint);
 
     }
+
+    private void drawFilledCircle(Canvas canvas, Paint paint, Point center,
+                                int radius) {
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(center.x, center.y, radius, paint);
+
+    }
+
 
     private void drawHollowRect(Canvas canvas, Paint paint, Point topLeftPoint,
             Point bottomRightPoint) {
