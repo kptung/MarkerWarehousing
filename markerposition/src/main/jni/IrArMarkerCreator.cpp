957c8aaba6e71164e796b06769b19d9b59ceb4f1
kptung <kptung@aa.bb.cc>
2018-04-23 13:58:06 +0800

    [Modify] Update the random ID generator + exclude ID generator.

--- markerposition/src/main/jni/IrArMarkerCreator.cpp
+++ markerposition/src/main/jni/IrArMarkerCreator.cpp
@@ -11,9 +11,9 @@ using namespace cv;
 using namespace std;
 
 /************************************************************************/
-/*  Aruco marker generator with the given marker length in pixels       */
+/*  Aruco marker generator with the given marker length in cm       */
 /*  Author: opencv_contrib                                              */
-/*  Modified: kptung, 2017/11/09                                        */
+/*  Modified: kptung, 2018/04/20                                        */
 /************************************************************************/
 void useage()
 {
@@ -22,7 +22,7 @@ void useage()
 	cerr << "\t<-r|g|b|c [b] [g] [r]> marker in color Red/Green/Blue/custom" << endl;
 	cerr << "\t<-exclude [exnum] [ID1] [ID2] ....> exclude IDs" << endl;
 	cerr << "\t<-input [innum] [ID1] [ID2] ....> input IDs" << endl;
-	cerr << "\t<-DPI [dpi]> Dots Per Inch (72:screen, 300:printer)" << endl;
+	cerr << "\t<-DPI [dpi]> Dots Per Inch (74:screen, 300:printer)" << endl;
 	exit(1);
 }
 
@@ -35,19 +35,18 @@ int main(int argc, char *argv[])
 	bool exflag = false;
 
 	cv::Vec3b color;
-	std::vector<int> excludeIdList;
-	std::vector<int> markerIdList;
 	int mnum = -1;	// marke_num
 	int msize = -1; // marker_size in cm
-	int exnum = -1; // exclude marker number
-	int innum = -1; // input marker number
 	float DPI = 74.2; // Dot Per Inch (defalt:74)
 	std::ostringstream outFolder; // output_folder
+	std::ostringstream output; // output file
 	cv::Mat markerImg; // output_image
-	int dictionaryId = 16; // pre-defined dictionary
 	int borderBits = 1; // marker border
-	std::ostringstream output;
 
+	int dictionaryId = 16; // pre-defined dictionary
+	int mlen = (dictionaryId == 16) ? 1024 : -1; // total 1024 markers
+	std::vector<int> mDBase(mlen, -1);
+	
 	if (argc < 3) {
 		useage();
 	}
@@ -119,12 +118,12 @@ int main(int argc, char *argv[])
 			exflag = true;
 			argc--;
 			argv++;
-			exnum = atoi(argv[1]);
+			int num = atoi(argv[1]);
 			argc--;
 			argv++;
-			for(int i = 0; i < exnum; i++)
+			for (int i = 0; i < num; i++)
 			{
-				excludeIdList.push_back(atoi(argv[1]));
+				mDBase.at(atoi(argv[1])) = 2;
 				argc--;
 				argv++;
 			}
@@ -134,12 +133,12 @@ int main(int argc, char *argv[])
 			inflag = true;
 			argc--;
 			argv++;
-			innum = atoi(argv[1]);
+			int num = atoi(argv[1]);
 			argc--;
 			argv++;
-			for (int i = 0; i < innum; i++)
+			for(int i = 0; i < num; i++)
 			{
-				markerIdList.push_back(atoi(argv[1]));
+				mDBase.at(atoi(argv[1])) = 1;
 				argc--;
 				argv++;
 			}
@@ -147,118 +146,80 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	// marker size generator
+	// marker size calculator
 	int markerSize = round(msize * DPI / 2.54);
 
 	// random marker generator
 	if (!inflag)
 	{
-		//initialize random seed
-		srand(time(NULL));
 		for (int i = 0; i < mnum; i++)
 		{
-			int id = round(rand() % 1024);
-			if (!exflag)
-			{
-				bool flag = false;
-				if (markerIdList.size() == 0)
-				{
-					markerIdList.push_back(id);
-					flag = false;
-				}
-				else
-					for (int j = 0; j < markerIdList.size(); j++)
-					{
-						while (flag)
-						{
-							if (id != markerIdList.at(j))
-							{
-								markerIdList.push_back(id);
-								flag = false;
-							}
-							else
-							{
-								flag = true;
-								id = round(rand() % 1024);
-							}
-						}
-					}
-					
-			}	
-			else 
-			{
-				bool flag = false;
-				for (int j = 0; j < exnum; j++)
-				{
-					while (flag)
-					{
-						if (id != excludeIdList.at(j))
-						{
-							markerIdList.push_back(id);
-							flag = false;
-						}
-						else
-						{
-							flag = true;
-							id = round(rand() % 1024);
-						}
-					}
-				}
-			}
+			//initialize random seed
+			srand(time(NULL));
+			int id = round(rand() % mlen);
+			while (mDBase.at(id) != -1)
+				id = round(rand() % mlen);
+			if (mDBase.at(id) == -1)
+				mDBase.at(id) = 1;
 		}
 	}
 	
 	cv::Ptr<cv::aruco::Dictionary> dictionary =
 		cv::aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
 
-	for (int i = 0; i < markerIdList.size(); i++)
+	for (int i = 0; i < mDBase.size(); i++)
 	{
-		aruco::drawMarker(dictionary, markerIdList.at(i), markerSize, markerImg, borderBits);
-		// convert bit-color: black->white, white->black
-		cv::bitwise_not(markerImg, markerImg);
-		// add border
-		int top = (int)(0.15*markerImg.rows), bottom = (int)(0.15*markerImg.rows);
-		int left = (int)(0.15*markerImg.cols), right = (int)(0.15*markerImg.cols);
-		int borderType = BORDER_CONSTANT;
-		copyMakeBorder(markerImg, markerImg, top, bottom, left, right, borderType, Scalar(0, 0, 0));
-		// make a color marker
-		cv::Mat colorMarker(markerImg.size(), CV_8UC3);
-		if (colorflag)
+		if (mDBase.at(i) == 1) 
 		{
-			for (int y = 0; y < markerImg.rows; y++)
-				for (int x = 0; x < markerImg.cols; x++)
-				{
+			aruco::drawMarker(dictionary, i, markerSize, markerImg, borderBits);
+			// convert bit-color: black->white, white->black
+			cv::bitwise_not(markerImg, markerImg);
+			// add border
+			int top = (int)(0.15*markerImg.rows), bottom = (int)(0.15*markerImg.rows);
+			int left = (int)(0.15*markerImg.cols), right = (int)(0.15*markerImg.cols);
+			int borderType = BORDER_CONSTANT;
+			copyMakeBorder(markerImg, markerImg, top, bottom, left, right, borderType, Scalar(0, 0, 0));
+			// make a color marker
+			cv::Mat colorMarker(markerImg.size(), CV_8UC3);
+			if (colorflag)
+			{
+				for (int y = 0; y < markerImg.rows; y++)
+					for (int x = 0; x < markerImg.cols; x++)
+					{
 					cv:Scalar intensity = markerImg.at<uchar>(y, x);
-					if (intensity.val[0] == 255)
-						colorMarker.at<Vec3b>(y, x) = cv::Vec3b(255, 255, 255);
-					if (intensity.val[0] == 0)
-						colorMarker.at<Vec3b>(y, x) = color;
-				}
-			colorMarker.copyTo(markerImg);
-		}
+						if (intensity.val[0] == 255)
+							colorMarker.at<Vec3b>(y, x) = cv::Vec3b(255, 255, 255);
+						if (intensity.val[0] == 0)
+							colorMarker.at<Vec3b>(y, x) = color;
+					}
+				colorMarker.copyTo(markerImg);
+			}
 
-		// RotateFlags 
-		// ROTATE_90_CLOCKWISE = 0, //Rotate 90 degrees clockwise
-		// ROTATE_180 = 1, //Rotate 180 degrees clockwise
-		// ROTATE_90_COUNTERCLOCKWISE = 2, //Rotate 270 degrees clockwise
-		if (rotflag)
-		{
-			cv::Mat tmp;
-			markerImg.copyTo(tmp);
-			cv::rotate(markerImg, tmp, ROTATE_90_CLOCKWISE);
-			cv::rotate(markerImg, tmp, ROTATE_180);
-			cv::rotate(markerImg, tmp, ROTATE_90_COUNTERCLOCKWISE);
-		}
+			// RotateFlags 
+			// ROTATE_90_CLOCKWISE = 0, //Rotate 90 degrees clockwise
+			// ROTATE_180 = 1, //Rotate 180 degrees clockwise
+			// ROTATE_90_COUNTERCLOCKWISE = 2, //Rotate 270 degrees clockwise
+			if (rotflag)
+			{
+				cv::Mat tmp;
+				markerImg.copyTo(tmp);
+				cv::rotate(markerImg, tmp, ROTATE_90_CLOCKWISE);
+				cv::rotate(markerImg, tmp, ROTATE_180);
+				cv::rotate(markerImg, tmp, ROTATE_90_COUNTERCLOCKWISE);
+			}
 
-		if (showImage) {
-			cv::imshow("marker", markerImg);
-			waitKey(0);
-		}
+			if (showImage) {
+				cv::imshow("marker", markerImg);
+				waitKey(0);
+			}
 
-		output << outFolder.str() << markerIdList.at(i) << ".jpg";
-		cv::imwrite(output.str(), markerImg);
-		output.str("");
-		output.clear();
+			output << outFolder.str() << i << ".jpg";
+			cv::imwrite(output.str(), markerImg);
+			output.str("");
+			output.clear();
+		}
+		
 	}
+		
 	return 0;
 }
